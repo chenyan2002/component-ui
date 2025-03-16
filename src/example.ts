86c58df187310c8db5cf7961c3ae28f8c7713c3e
Yan Chen <yan.chen@fastly.com>
2025-03-16 16:15:41 -0700

    make a deploy

--- src/example.ts
+++
@@ -1,5 +0,0 @@
-const IDL = await import('./wit');
-export const Op = IDL.Enum(['add']);
-export default IDL.Interface('docs:calculator/calculate@0.1.0', {
-  'evalExpression': IDL.Func([['op', Op], ['x', IDL.U32], ['y', IDL.U32], ], [IDL.U32])
-})
